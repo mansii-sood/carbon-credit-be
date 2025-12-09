593edda06eb02053a5410ea777a430e2422207d5
mansii-sood <mansisood340@gmail.com>
2025-12-10 01:31:54 +0530

    second commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "carbon_credit"
+    });
+    console.log("MongoDB Connected");
   } catch (err) {
-    console.error("DB connection failed", err);
+    console.error("DB Connection Error:", err.message);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => res.send("API running"));
 
 connectDB();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server ${PORT}`));
