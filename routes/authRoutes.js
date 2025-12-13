84c8f266c08f3efd366011f0f7fd4c68e79a69e2
mansii-sood <mansisood340@gmail.com>
2025-12-14 00:27:58 +0530

    Add .gitignore and remove sensitive files

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import auth from "../middleware/auth.js";
 import { registerUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
